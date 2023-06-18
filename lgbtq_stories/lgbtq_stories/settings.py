0317b4c7fe3fdaa1190c7ce65d9593486561eece
Raghav Limbani <itsrgcodes@gmail.com>
2023-06-18 16:22:06 +0530

    UI changes

--- lgbtq_stories/lgbtq_stories/settings.py
+++ lgbtq_stories/lgbtq_stories/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    #'defualt.apps.DefaultConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,10 @@ ROOT_URLCONF = 'lgbtq_stories.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'default', 'templates/default'),
+            os.path.join(BASE_DIR, 'users', 'templates/users')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +81,11 @@ WSGI_APPLICATION = 'lgbtq_stories.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'users',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
     }
 }
 
--- lgbtq_stories/users/models.py
+++ lgbtq_stories/users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class UserInformation(models.Model):
+    fname = models.CharField(max_length=12)
+    lname = models.CharField(max_length=12)
+    # GENDER FIELD WITH CHOICES
+    MALE = 'ML'
+    FEMALE = 'FM'
+    NON_BINARY = 'NB'
+    PREFER_NOT_TO_SAY = 'PNTS'
+    OTHER = 'OTHR'
+    GENDER_CHOICES = [
+        (MALE,"Male"),
+        (FEMALE,"Female"),
+        (NON_BINARY,"Non-Binary"),
+        (PREFER_NOT_TO_SAY,"Prefer-Not-to-Say"),
+        (OTHER,"Other"),
+    ]
+    gender = models.CharField(
+        max_length=12,
+        choices=GENDER_CHOICES,
+        default=PREFER_NOT_TO_SAY,
+    )
+    dob = models.DateField()
+    email = models.EmailField()
+    ph_num = models.IntegerField()
+
+class Credentials(models.Model):
+    username = models.CharField(max_length=12)
+    password = models.CharField(max_length=12)
